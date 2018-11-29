9a552ff7cafd300408ffbdacafa572cf907a72f4
travisbright <travis.bright1994@gmail.com>
2018-11-29 01:35:39 -0500

    added posts and links to footer for the 105-106 challenge, committing and pushing but will NOT merge to master.

--- assests/js/app.js
+++ assests/js/app.js
@@ -31,8 +31,6 @@ function init() {
       getPage(52);
       $("#loaderDiv").fadeOut("slow");
 
-
-
     },
     error: function() {
       console.log('all is not good');
@@ -40,7 +38,25 @@ function init() {
   })
 }
 
+$.ajax({
+  method: 'GET',
+  url: 'http://me.tbtestsite.com/wp-json/wp-api-menus/v2/menus/3',
+  dataType: 'json',
+  success: function(data) {
 
+    var menu = menuBuilder(data.items, 'genLinks', 'footer-ul');
+    $('#genLinks').replaceWith(menu);
+    $('#genLinks li a').click(function(){
+      getPage($(this).data("pgid"));
+    });
+
+  },
+  error: function() {
+    console.log('something went wrong with the general links menu');
+  }
+});
+
+getPosts();
 
 
 function menuBuilder(obj) {
@@ -88,6 +104,27 @@ function getPage(obj) {
     },
     error: function() {
       console.log('bad');
+    }
+  });
+}
+
+function getPosts() {
+
+  $.ajax({
+    method: 'GET',
+    url:'http://me.tbtestsite.com/wp-json/wp/v2/posts?orderby=date&order=desc&per_page=3',
+    dataType: 'json',
+    success: function (data) {
+      $("#latestPosts").html('<p id="postLdr"><i class="fa fa-cogs"></i>Loading Posts</p>');
+      data.forEach(function (item) {
+        var myDate= new Date(item.date);
+        $("#latestPosts").prepend('<p>' + item.title.rendered + '<span>' + myDate.getMonth() + '-' + myDate.getDay() + '-' + myDate.getFullYear () + '</span></p>');
+
+      });
+      $("postLdr").remove();
+    },
+    error: function() {
+      console.log('something went wrong with post call');
     }
   });
 }
